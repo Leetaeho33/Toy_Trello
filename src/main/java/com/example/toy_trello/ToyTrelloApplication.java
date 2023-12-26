07f3e69bb5cb8e7733a9d22784f3e0de1f005586
taeholee <lth1993@naver.com>
2023-12-26 19:10:36 +0900

    init commit

---
+++ src/main/java/com/example/toy_trello/ToyTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.toy_trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyTrelloApplication.class, args);
+    }
+
+}
