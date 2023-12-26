7b420e272d2763b41c62a114e112142b1218d6fa
taeholee <lth1993@naver.com>
2023-12-26 20:16:31 +0900

    FEAT : BaseEntity 셋업(날짜/시간)

---
+++ src/main/java/com/example/toy_trello/domain/util/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.toy_trello.domain.util;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
