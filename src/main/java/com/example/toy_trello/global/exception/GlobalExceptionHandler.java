6c6a8aa5d46b42bc859b0c0fd53a21a3131cdd59
jimyung0418 <wjdwlaud0418@naver.com>
2023-12-27 18:10:01 +0900

    FEAT: GlobalExceptionHandler MethodArgumentNotValidException 던지기 추가

--- src/main/java/com/example/toy_trello/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/toy_trello/global/exception/GlobalExceptionHandler.java
@@ -3,8 +3,14 @@ package com.example.toy_trello.global.exception;
 import com.example.toy_trello.global.dto.CommonResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +19,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CommonResponseDto> handleException(IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).
                 body(new CommonResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<CommonResponseDto> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = new ArrayList<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errorMessages.add(fieldError.getDefaultMessage());
+        }
+
+        String errorMessage = String.join(" ", errorMessages);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new CommonResponseDto(errorMessage, HttpStatus.BAD_REQUEST.value()));
     }
 }
