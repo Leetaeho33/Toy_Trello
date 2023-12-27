31e76f1d96738372f8e771607382c037ccc94d0e
jimyung0418 <wjdwlaud0418@naver.com>
2023-12-27 18:10:50 +0900

    FIX: @EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/example/toy_trello/ToyTrelloApplication.java
+++ src/main/java/com/example/toy_trello/ToyTrelloApplication.java
@@ -2,8 +2,10 @@ package com.example.toy_trello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ToyTrelloApplication {
 
     public static void main(String[] args) {
