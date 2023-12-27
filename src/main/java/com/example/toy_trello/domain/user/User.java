fb68310c46de92fc4b5ce7456ed9818bc0c9249a
jimyung0418 <wjdwlaud0418@naver.com>
2023-12-27 10:17:15 +0900

    FEAT: User entity 생성

---
+++ src/main/java/com/example/toy_trello/domain/user/User.java
@@ -0,0 +1,23 @@
+package com.example.toy_trello.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    private String password;
+    private String intro;
+    private String email;
+}
