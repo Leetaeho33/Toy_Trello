a340297dbf2af80b699e7680ca4ac0ad65dcf725
taeholee <lth1993@naver.com>
2023-12-29 17:48:50 +0900

    REFECTOR : TeamController RequestMapping 변경 /api/teams -> /vi/api/teams

--- src/main/java/com/example/toy_trello/domain/team/controller/TeamController.java
+++ src/main/java/com/example/toy_trello/domain/team/controller/TeamController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/teams")
+@RequestMapping("/vi/api/teams")
 public class TeamController {
     private final TeamService teamService;
 
