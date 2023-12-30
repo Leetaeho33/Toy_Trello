25b93be9c6b94f63fecd3dd479976ab7da450471
taeholee <lth1993@naver.com>
2023-12-30 22:12:01 +0900

    FEAT : 팀 탈퇴 기능 구현(2)

--- src/main/java/com/example/toy_trello/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/toy_trello/domain/member/repository/MemberRepository.java
@@ -5,9 +5,12 @@ import com.example.toy_trello.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
     List<Member> findByTeam_Id(Long teamId);
+
+    Optional<Member> findFirstByTeamId(Long teamId);
 }
