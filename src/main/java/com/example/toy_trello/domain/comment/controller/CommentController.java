213ea764fe82bb3d7be8d5515a95f7f714d598fb
taeholee <lth1993@naver.com>
2024-01-02 14:02:41 +0900

    REFACTOR : 클래스 패키지 위치 변경

--- src/main/java/com/example/toy_trello/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/toy_trello/domain/comment/controller/CommentController.java
@@ -1,21 +1,17 @@
-package com.example.toy_trello.domain.comment;
+package com.example.toy_trello.domain.comment.controller;
 
+import com.example.toy_trello.domain.comment.service.CommentService;
 import com.example.toy_trello.domain.comment.dto.CommentRequestDto;
 import com.example.toy_trello.domain.comment.dto.CommentResponseDto;
-import com.example.toy_trello.domain.comment.dto.PageDto;
 import com.example.toy_trello.global.dto.CommonResponseDto;
 import com.example.toy_trello.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.core.annotation.AuthenticationPrincipal;
 //import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import javax.print.attribute.HashPrintJobAttributeSet;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/comments")
